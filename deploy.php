40f639b85d6fdea95e51ed1d91fbd37ffc7fb912
cremirdevio <josephajibodu@gmail.com>
2023-03-05 13:08:47 -0800

    chore: deployer added

---
+++ deploy.php
@@ -0,0 +1,39 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+set('application', 'Suraksha');
+set('repository', '');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('prod')
+    ->set('port', '65002')
+    ->set('hostname', '82.180.175.94')
+    ->set('remote_user', 'u737857919')
+    ->set('deploy_path', '~/domains/surakshaproject.in/public_html/prod-api');
+
+// Tasks
+
+task('deploy', [
+    'deploy:prepare',
+    'deploy:vendors',
+    'artisan:storage:link',
+    'artisan:config:cache',
+    'artisan:view:cache',
+    'artisan:migrate',
+//    'artisan:scribe:generate',
+    'artisan:down',
+    'deploy:publish',
+    'artisan:up',
+]);
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
